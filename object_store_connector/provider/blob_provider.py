ea4429d9c60a4cefe99d38c041f9252fa326495c
Ravi Mula <ravismula@users.noreply.github.com>
2024-04-25 15:25:12 +0530

    Sanketika-Obsrv/issue-tracker#64: s3 connector

---
+++ object_store_connector/provider/blob_provider.py
@@ -0,0 +1,30 @@
+from abc import ABC, abstractmethod
+from typing import Any, Dict, List
+from pyspark.sql import SparkSession, DataFrame
+from obsrv.connector import MetricsCollector
+
+from models.object_info import ObjectInfo, Tag
+
+class JobConfig:
+    pass
+
+class BlobProvider(ABC):
+    @abstractmethod
+    def get_spark_config(self, config: JobConfig) -> Dict[str, Any]:
+        pass
+
+    @abstractmethod
+    def fetch_tags(self, objectPath: str, metrics_collector: MetricsCollector, config: Dict[str, Any] = None) -> List[Tag]:
+        pass
+
+    @abstractmethod
+    def update_tag(self, object: ObjectInfo, tags: List[Tag], metrics_collector: MetricsCollector, config: Dict[str, Any] = None) -> bool:
+        pass
+
+    @abstractmethod
+    def fetch_objects(self, metrics_collector: MetricsCollector, connectorConfig: Dict[str, Any] = None) -> List[ObjectInfo]:
+        pass
+
+    @abstractmethod
+    def read_object(self, objectPath: str, metrics_collector: MetricsCollector, config: Dict[str, Any] = None, sc: SparkSession = None) -> DataFrame:
+        pass
